298db47032a1397c6cb8f0604b36d40bb7b9af4a
Osama Mohammed <docosmo010@gmail.com>
2025-11-12 17:49:21 +0200

    feat(backend): Remove payload :(

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,22 +1,10 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
-import payload from "payload";
-import config from "./payload.config";
 
 const app = express();
 
-const start = async () => {
-  await payload.init({
-    config,
-    onInit: () => {
-      payload.logger.info(`Admin URL: ${payload.getAdminURL()}`);
-    },
-  });
-
-  app.listen(4000, () => {
-    console.log(`🚀 Payload CMS running on http://localhost:4000`);
-  });
-};
-
-start();
+app.listen(4000, () => {
+  console.log(`Express running on http://localhost:4000`);
+});
